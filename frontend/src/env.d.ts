0b6cc8b51098114347e37d7eae090efb6a52a1e1
masx200 <masx200@qq.com>
2026-05-08 16:22:42 +0800

    feat: 添加对 Monaco 编辑器的语言模块和 API 的声明

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -5,3 +5,19 @@ declare module '*.vue' {
     const component: DefineComponent<{}, {}, any>;
     export default component;
 }
+
+declare module 'monaco-editor/esm/vs/language/json/monaco.contribution' {}
+declare module 'monaco-editor/esm/vs/language/css/monaco.contribution' {}
+declare module 'monaco-editor/esm/vs/language/html/monaco.contribution' {}
+declare module 'monaco-editor/esm/vs/language/typescript/monaco.contribution' {}
+declare module 'monaco-editor/esm/vs/basic-languages/_.contribution.js' {}
+declare module 'monaco-editor/esm/vs/editor/contrib/folding/browser/folding.js' {}
+declare module 'monaco-editor/esm/vs/editor/contrib/contextmenu/browser/contextmenu.js' {}
+declare module 'monaco-editor/esm/vs/editor/contrib/clipboard/browser/clipboard.js' {}
+declare module 'monaco-editor/esm/vs/editor/contrib/dropOrPasteInto/browser/copyPasteContribution.js' {}
+declare module 'monaco-editor/esm/vs/editor/contrib/find/browser/findController.js' {}
+declare module 'monaco-editor/esm/vs/editor/contrib/multicursor/browser/multicursor.js' {}
+declare module 'monaco-editor/esm/vs/editor/standalone/browser/quickAccess/standaloneCommandsQuickAccess.js' {}
+declare module 'monaco-editor/esm/vs/editor/editor.api' {
+    export * from 'monaco-editor';
+}
